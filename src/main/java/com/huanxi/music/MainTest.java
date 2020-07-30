d33c94841173d3cf389aa56e68625316a856ef4d
huangjiawei <1355473748@qq.com>
2020-07-30 22:12:35 +0800

    特殊路径修改

---
+++ src/main/java/com/huanxi/music/MainTest.java
@@ -0,0 +1,13 @@
+package com.huanxi.music;
+
+/**
+ * @author huanxi
+ * @date 2020/7/30 10:08 下午
+ * @email 1355473748@qq.com
+ */
+
+public class MainTest {
+    public static void main(String[] args) {
+
+    }
+}
